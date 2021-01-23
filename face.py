8f3139a918c527cc820d14c790caccfe4ce55294
Bahadır <64895948+baho0@users.noreply.github.com>
2021-01-23 15:34:07 +0300

    Update face.py

--- face.py
+++ face.py
@@ -9,6 +9,11 @@ while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cv2.imshow("gray",gray)
+    #faces = face_cascade.detectMultiScale(frame,1.3,5)
+    for (x,y,w,h) in face_cascade:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        #roi_gray = gray[y:y+h]
+
     if cv2.waitKey(1) & 0XFF == ord('q'):
         break
 cap.release()
